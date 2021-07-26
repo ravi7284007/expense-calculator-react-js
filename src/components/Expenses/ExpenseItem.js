8bff2a2e8c1d4721532a8394268c1419412058c8
Ravi <ravi7284007@gmail.com>
2021-07-27 02:23:03 +0530

    changes on expenses file

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,7 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
   // let title = props.title;
+  console.log(props);
 
   const clickHandler = () => {
     setTitle("Updated!");
