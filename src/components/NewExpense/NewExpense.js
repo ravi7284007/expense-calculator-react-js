b2b6bfff0d31fd4ea6b23811e2aaa71b7274d617
Ravi <ravi7284007@gmail.com>
2021-07-27 02:09:31 +0530

    changes in Expenses files

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,11 +3,18 @@ import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
 
-const NewExpense = () => {
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
